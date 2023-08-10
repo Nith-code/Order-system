445da65a9ed75e2e2785b820a3e21be22effe864
Nith-code <phanith.lim22@kit.edu.kh>
2023-08-10 13:27:42 +0700

    Create coffee_system.py

---
+++ coffee_system.py
@@ -0,0 +1,6 @@
+list_of_drink = {1 :'Ice latte',2 :'Green tea',3 :'Black coffee',4 : 'Cappucino', 5 : 'American'}
+for i in list_of_drink:
+    print(list_of_drink[i])
+order = input('What do you want to order? ')
+if order == list_of_drink[i]:
+    print('')
